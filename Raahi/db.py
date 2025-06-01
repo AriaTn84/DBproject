e764f334d527a7bf5ee06280991bc778103cb207
Zahra <zahra.aghaei@email.kntu.ac.ir>
2025-06-01 18:22:29 +0330

    venv

--- Raahi/db.py
+++ Raahi/db.py
@@ -7,7 +7,7 @@ def get_db_connection():
             host='localhost',
             database='raahi_db',
             user='root',
-            password='Mobin@2005'
+            password='1234'
         )
         if connection.is_connected():
             print("Connected to MySQL database")
